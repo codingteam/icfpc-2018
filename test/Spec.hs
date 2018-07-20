83187a36ac8d61f002f6f5a29f515ac1f035faa9
Alexander Batischev <eual.jp@gmail.com>
2018-07-20 23:49:19 +0300

    Move tests to Tasty framework
    Unlike asserts, Tasty provides nicely formatted output for failing
    testcases.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,5 +1,8 @@
 {-# LANGUAGE BinaryLiterals #-}
 
+import Test.Tasty
+import Test.Tasty.HUnit
+
 import Data.Bits
 import Data.Bytes.Put
 import Data.Bytes.Get
@@ -13,18 +16,35 @@ assert _ True = return ()
 assert message False = fail message
 
 main :: IO ()
-main = do
-  assert "encode.SMove.1" $ testEncode (SMove (LongLinDiff X 12)) == "00010100 00011011"
-  assert "encode.SMove.2" $ testEncode (SMove (LongLinDiff Z (-4))) == "00110100 00001011"
+main = defaultMain tests
 
-  assert "encode.LMove.1" $ testEncode (LMove (ShortLinDiff X 3) (ShortLinDiff Y (-5))) == "10011100 00001000"
-  assert "encode.LMove.2" $ testEncode (LMove (ShortLinDiff Y (-2)) (ShortLinDiff Z 2)) == "11101100 01110011"
+tests :: TestTree
+tests = testGroup "Tests" [specExamples]
 
-  assert "encode.FusionP" $ testEncode (FusionP (NearDiff (-1) 1 0)) == "00111111"
+specExamples :: TestTree
+specExamples = testGroup "Specification examples"
+  [
+    testCase "encode.SMove.1" $
+      testEncode (SMove (LongLinDiff X 12)) @?= "00010100 00011011"
+  , testCase "encode.SMove.2" $
+      testEncode (SMove (LongLinDiff Z (-4))) @?= "00110100 00001011"
 
-  assert "encode.FusionS" $ testEncode (FusionS (NearDiff 1 (-1) 0)) == "10011110"
+  , testCase "encode.LMove.1" $
+      testEncode (LMove (ShortLinDiff X 3) (ShortLinDiff Y (-5))) @?= "10011100 00001000"
+  , testCase "encode.LMove.2" $
+      testEncode (LMove (ShortLinDiff Y (-2)) (ShortLinDiff Z 2)) @?= "11101100 01110011"
 
-  assert "encode.Fission" $ testEncode (Fission (NearDiff 0 0 1) 5) == "01110101 00000101"
+  , testCase "encode.FusionP" $
+      testEncode (FusionP (NearDiff (-1) 1 0)) @?= "00111111"
 
-  assert "encode.Fill" $ testEncode (Fill (NearDiff 0 (-1) 0)) == "01010011"
-  
+  , testCase "encode.FusionS" $
+      testEncode (FusionS (NearDiff 1 (-1) 0)) @?= "10011110"
+
+  , testCase "encode.Fission" $
+      testEncode (Fission (NearDiff 0 0 1) 5) @?= "01110101 00000101"
+
+  , testCase "encode.Fill" $
+      testEncode (Fill (NearDiff 0 (-1) 0)) @?= "01010011"
+  ]
+
+
