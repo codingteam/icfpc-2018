118b72dd61d3ef4c3e2a3361eb9438e9a7987c02
Ilya Portnov <portnov@bk.ru>
2018-07-21 21:02:07 +0500

    Switch to High mode only when needed.
    refs #5.
    
    on task #19, this reduced energy from 35617050588 to 3564378588 (10x
    approx.).

--- src/Algorithms.hs
+++ src/Algorithms.hs
@@ -6,6 +6,8 @@ import Control.Monad.State
 import Data.Word
 import Data.Binary (decodeFile)
 import Text.Printf
+import qualified Data.Array.BitArray as BA
+import qualified Data.Set as S
 
 import Trace
 import Sim
@@ -44,8 +46,29 @@ fill bid dir p@(x,y,z) = do
     (neighbour, diff) <- findFreeNeighbour p
     let diff' = negateNear diff
     move bid neighbour
+    grounded <- willBeGrounded p
+    unless grounded $
+      setHarmonics bid High
     issueFill bid diff'
+    setHarmonics bid Low
     step
+
+-- This would be too slow
+-- isGrounded :: P3 -> Generator Bool
+-- isGrounded p = do
+--     matrix <- gets gsFilled
+--     return $ go matrix S.empty p
+--   where
+--     go _ _ (_,0,_) = True
+--     go m visited p@(x,y,z) =
+--       case m BA.!? p of
+--         Nothing -> return False
+--         Just False -> return False
+--         Just True ->
+--           let neighbours = [(x+1, y, z), (x, y+1, z), (x, y, z+1),
+--                             (x-1, y, z), (x, y-1, z), (x, y, z-1)]
+--               nonVisited = [neighbour | neighbour `S.notMember` visited]
+--           in  or [go m (S.insert neighbour visited) neighbour | neighbour <- nonVisited]
 
 makeLine :: Direction -> Resolution -> Word8 -> Word8 -> [P3]
 makeLine dir r y z = 
@@ -98,10 +121,11 @@ dumbHighSolver modelPath tracePath = do
   model <- decodeFile modelPath
   let trace = makeTrace model $ do
                 let bid = 0
-                issue bid Flip
+                -- issueFlip bid
                 dumbFill bid
                 move bid (0,0,0)
-                issue bid Flip
+                -- issueFlip bid
                 issue bid Halt
+  print trace
   writeTrace tracePath trace
 
