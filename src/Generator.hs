codingteam/icfpc-2018

commit 7ed20d8623ccb646a88a027caafb5ab9b32a4611

Introduce separate type for offsets

src/Generator.hs
@@ -5,6 +5,7 @@ import Control.Monad
 import Control.Monad.State
 import Data.Maybe
 import Data.Array
+import Data.Int
 import qualified Data.Array.BitArray as BA
 
 import Trace
@@ -28,6 +29,9 @@ type BotTrace = [Command]
 type Step = Int
 
 type AliveBot = BID
+
+-- | Offset relative to some point in the matrix.
+type P3d = (Int16, Int16, Int16)
 
 data GeneratorState = GS {
     gsModel :: ModelFile,
@@ -99,68 +103,74 @@ step = do
     let traces' = traces // updates
     modify $ \st -> st {gsStepNumber = n', gsTraces = traces'}
 
-substractCmd :: P3 -> Command -> P3
-substractCmd (dx, dy, dz) (SMove (LongLinDiff X dx1)) = (dx-dx1, dy, dz)
-substractCmd (dx, dy, dz) (SMove (LongLinDiff Y dy1)) = (dx, dy-dy1, dz)
-substractCmd (dx, dy, dz) (SMove (LongLinDiff Z dz1)) = (dx, dy, dz-dz1)
-substractCmd (dx, dy, dz) (LMove (ShortLinDiff X dx1) (ShortLinDiff X dx2)) = (dx-dx1-dx2, dy, dz)
-substractCmd (dx, dy, dz) (LMove (ShortLinDiff Y dy1) (ShortLinDiff Y dy2)) = (dx, dy-dy1-dy2, dz)
-substractCmd (dx, dy, dz) (LMove (ShortLinDiff Z dz1) (ShortLinDiff Z dz2)) = (dx, dy, dz-dz1-dz2)
-substractCmd (dx, dy, dz) (LMove (ShortLinDiff X dx1) (ShortLinDiff Y dy2)) = (dx-dx1, dy-dy2, dz)
-substractCmd (dx, dy, dz) (LMove (ShortLinDiff X dx1) (ShortLinDiff Z dz2)) = (dx-dx1, dy, dz-dz2)
-substractCmd (dx, dy, dz) (LMove (ShortLinDiff Y dy1) (ShortLinDiff Z dz2)) = (dx, dy-dy1, dz-dz2)
+substractCmd :: P3d -> Command -> P3d
+substractCmd (dx, dy, dz) (SMove (LongLinDiff X dx1)) = (dx-(fromIntegral dx1), dy, dz)
+substractCmd (dx, dy, dz) (SMove (LongLinDiff Y dy1)) = (dx, dy-(fromIntegral dy1), dz)
+substractCmd (dx, dy, dz) (SMove (LongLinDiff Z dz1)) = (dx, dy, dz-(fromIntegral dz1))
+substractCmd (dx, dy, dz) (LMove (ShortLinDiff X dx1) (ShortLinDiff X dx2)) = (dx-(fromIntegral dx1)-(fromIntegral dx2), dy, dz)
+substractCmd (dx, dy, dz) (LMove (ShortLinDiff Y dy1) (ShortLinDiff Y dy2)) = (dx, dy-(fromIntegral dy1)-(fromIntegral dy2), dz)
+substractCmd (dx, dy, dz) (LMove (ShortLinDiff Z dz1) (ShortLinDiff Z dz2)) = (dx, dy, dz-(fromIntegral dz1)-(fromIntegral dz2))
+substractCmd (dx, dy, dz) (LMove (ShortLinDiff X dx1) (ShortLinDiff Y dy2)) = (dx-(fromIntegral dx1), dy-(fromIntegral dy2), dz)
+substractCmd (dx, dy, dz) (LMove (ShortLinDiff X dx1) (ShortLinDiff Z dz2)) = (dx-(fromIntegral dx1), dy, dz-(fromIntegral dz2))
+substractCmd (dx, dy, dz) (LMove (ShortLinDiff Y dy1) (ShortLinDiff Z dz2)) = (dx, dy-(fromIntegral dy1), dz-(fromIntegral dz2))
 substractCmd p (LMove sld1 sld2) = substractCmd p (LMove sld2 sld1)
 substractCmd _ c = error $ "Impossible move command: " ++ show c
 
 origin :: P3
 origin = (0,0,0)
 
-extractMove :: P3 -> Either P3 (Command, P3)
+clamp :: Ord a => (a,  a) -> a -> a
+clamp (low, high) value
+  | value >= low && value <= high = max low (min value high)
+  | otherwise = error "clamp: value is outside of clamping range"
+
+extractMove :: P3d -> Either P3d (Command, P3d)
 extractMove p@(dx, dy, dz) =
-  case (dx /= 0, dy /= 0, dz /= 0) of
+  let clamp5 = clamp (-5, 5)
+  in case (dx /= 0, dy /= 0, dz /= 0) of
     (True, True, False) ->
-      let cmd = LMove (ShortLinDiff X (min dx 5)) (ShortLinDiff Y (min dy 5))
+      let cmd = LMove (ShortLinDiff X (fromIntegral $ clamp5 dx)) (ShortLinDiff Y (fromIntegral $ clamp5 dy))
           res = substractCmd p cmd
       in  Right (cmd, res)
     (True, False, True) ->
-      let cmd = LMove (ShortLinDiff X (min dx 5)) (ShortLinDiff Z (min dz 5))
+      let cmd = LMove (ShortLinDiff X (fromIntegral $ clamp5 dx)) (ShortLinDiff Z (fromIntegral $ clamp5 dz))
           res = substractCmd p cmd
       in  Right (cmd, res)
     (False, True, True) ->
-      let cmd = LMove (ShortLinDiff Y (min dy 5)) (ShortLinDiff Z (min dz 5))
+      let cmd = LMove (ShortLinDiff Y (fromIntegral $ clamp5 dy)) (ShortLinDiff Z (fromIntegral $ clamp5 dz))
           res = substractCmd p cmd
       in  Right (cmd, res)
     (True, False, False) ->
-      let cmd = SMove (LongLinDiff X $ min dx 15)
+      let cmd = SMove (LongLinDiff X $ fromIntegral $ clamp5 dx)
           res = substractCmd p cmd
       in  Right (cmd, res)
     (False, True, False) ->
-      let cmd = SMove (LongLinDiff Y $ min dy 15)
+      let cmd = SMove (LongLinDiff Y $ fromIntegral $ clamp5 dy)
           res = substractCmd p cmd
       in  Right (cmd, res)
     (False, False, True) ->
-      let cmd = SMove (LongLinDiff Z $ min dz 15)
+      let cmd = SMove (LongLinDiff Z $ fromIntegral $ clamp5 dz)
           res = substractCmd p cmd
       in  Right (cmd, res)
     _ -> Left p
-    
+
 -- | NOTE: This does not check if all intermediate voxels are free!
 -- We will need more clever algorithm.
-moveCommands :: P3 -> [Command]
+moveCommands :: P3d -> [Command]
 moveCommands (0,0,0) = []
 moveCommands p =
   case extractMove p of
-    Left p' -> if p' == origin
+    Left p'@(x, y, z) -> if x == 0 && y == 0 && z == 0
                  then []
                  else error $ "Cannot do such move: " ++ show p'
     Right (cmd, p') -> cmd : moveCommands p'
 
 -- | Move one bot in series of steps
 move :: BID -> P3 -> Generator ()
-move bid newPos@(nx, ny, nz) = do  
+move bid newPos@(nx, ny, nz) = do
   bot <- getBot bid
   let pos@(x,y,z) = _pos bot
-      diff = (nx-x, ny-y, nz-z)
+      diff = ((fromIntegral nx)-(fromIntegral x), (fromIntegral ny)-(fromIntegral y), (fromIntegral nz)-(fromIntegral z))
       commands = moveCommands diff
   forM_ commands $ \cmd -> do
       issue bid cmd

src/Trace.hs
@@ -8,6 +8,7 @@ import Data.Bits.Coded
 import Data.Bits.Coding
 import Data.Binary.Get (Get)
 import Data.Binary.Put (PutM)
+import Data.Int
 import qualified Data.ByteString.Lazy as L
 import Data.Word
 import Text.Printf
@@ -15,13 +16,13 @@ import Text.Printf
 data Axis = X | Y | Z
   deriving (Eq, Show, Enum)
 
-data ShortLinDiff = ShortLinDiff Axis Word8
+data ShortLinDiff = ShortLinDiff Axis Int8
   deriving (Eq, Show)
 
-data LongLinDiff = LongLinDiff Axis Word8
+data LongLinDiff = LongLinDiff Axis Int8
   deriving (Eq, Show)
 
-data NearDiff = NearDiff Word8 Word8 Word8
+data NearDiff = NearDiff Int8 Int8 Int8
   deriving (Eq, Show)
 
 data Command =

commit 43a68e51fff95dcd8403581d613c8711d9022353

Allow `SMove`s farther than 5 voxels
Fixes the bug I introduced in 7ed20d8623ccb646a88a027caafb5ab9b32a4611,
where I changed `min dx 15` to `clamp5 dx`.

src/Generator.hs
@@ -125,6 +125,7 @@ clamp (low, high) value
 extractMove :: P3d -> Either P3d (Command, P3d)
 extractMove p@(dx, dy, dz) =
   let clamp5 = clamp (-5, 5)
+      clamp15 = clamp (-15, 15)
   in case (dx /= 0, dy /= 0, dz /= 0) of
     (True, True, False) ->
       let cmd = LMove (ShortLinDiff X (fromIntegral $ clamp5 dx)) (ShortLinDiff Y (fromIntegral $ clamp5 dy))
@@ -139,15 +140,15 @@ extractMove p@(dx, dy, dz) =
           res = subtractCmd p cmd
       in  Right (cmd, res)
     (True, False, False) ->
-      let cmd = SMove (LongLinDiff X $ fromIntegral $ clamp5 dx)
+      let cmd = SMove (LongLinDiff X $ fromIntegral $ clamp15 dx)
           res = subtractCmd p cmd
       in  Right (cmd, res)
     (False, True, False) ->
-      let cmd = SMove (LongLinDiff Y $ fromIntegral $ clamp5 dy)
+      let cmd = SMove (LongLinDiff Y $ fromIntegral $ clamp15 dy)
           res = subtractCmd p cmd
       in  Right (cmd, res)
     (False, False, True) ->
-      let cmd = SMove (LongLinDiff Z $ fromIntegral $ clamp5 dz)
+      let cmd = SMove (LongLinDiff Z $ fromIntegral $ clamp15 dz)
           res = subtractCmd p cmd
       in  Right (cmd, res)
     _ -> Left p