f7e2dcf0513b588ff85fd1c3778335e0616c2622
Akon32 <akon32@rambler.ru>
2018-07-21 09:46:51 +0300

    + call dummy tracegen in main().

--- app/Main.hs
+++ app/Main.hs
@@ -4,10 +4,21 @@ import System.Environment
 
 import Trace
 import Model
+import TraceGens
+import Sim
+import Data.Array.BitArray
+import Data.Binary
+
+gentrace:: (BitArray P3 -> [Command]) -> FilePath -> FilePath -> IO()
+gentrace f infile outfile = do
+    modelFile <- decodeFile infile
+    let trace = f $ mfMatrix modelFile
+    writeTrace outfile trace
 
 main :: IO ()
 main = do
-  [cmd, file] <- getArgs
-  case cmd of
-    "display" -> displayModelFile file
+  args <- getArgs
+  case args of
+    ["display", file] -> displayModelFile file
+    ["gentrace", "dummy", infile, outfile] -> gentrace dummyTraceGen infile outfile
     _ -> fail "unknown command"
