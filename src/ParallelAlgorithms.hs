2b3850206f8cbb53fcff1224f933feafbf1285df
Friedrich von Never <friedrich@fornever.me>
2018-07-22 22:24:50 +0300

    Working fork algorithm for parallel (#16)

--- src/ParallelAlgorithms.hs
+++ src/ParallelAlgorithms.hs
@@ -6,7 +6,6 @@ import qualified Data.Array.BitArray as BitArray
 import qualified Data.List as List
 import Data.Ord
 import Data.Word
-import Debug.Trace
 import System.IO.Unsafe
 
 import Generator
@@ -23,6 +22,7 @@ getCurrentlyAliveBots = do
 
 raiseAll :: Word8 -> Generator ()
 raiseAll y = do
+  -- TODO: This should be simultaneous for all bots
   aliveBots <- getCurrentlyAliveBots
   forM_ aliveBots $ \bot -> do
     let (x, _, z) = _pos bot
@@ -38,96 +38,131 @@ getLayerLines layerNum = do
 
 getPreferredTotalBotCount :: Word8 -> Word8
 getPreferredTotalBotCount lineCount =
-  min maxBID lineCount
+  min 2 lineCount -- TODO: Replace 2 with maxBID
 
 isRootBot :: BotState -> Bool
 isRootBot b = _bid b == 0
 
-rootBotAdjustAction :: [Word8] -> Generator ()
-rootBotAdjustAction linesToFill = do
+rootBotAdjustAction :: Word8 -> [Word8] -> Generator ()
+rootBotAdjustAction layer linesToFill = do
   let preferredBotCount = getPreferredTotalBotCount $ fromIntegral $ length linesToFill
   aliveBots <- getCurrentlyAliveBots
-  let aliveBotCount = traceShowId $ trace "Alive count:" $ fromIntegral $ length aliveBots
-  let myself = traceShowId $ trace "myself:" $ head $ filter isRootBot aliveBots
+  model <- gets gsModel
+  let aliveBotCount = fromIntegral $ length aliveBots
+  let myself = head $ filter isRootBot aliveBots
   let isNextLineFree =
         let coord = nearPlus (_pos myself) $ NearDiff 0 0 1
-        in traceShowId $ trace "isNextLineFree" $ not $ any (\b -> _pos b == coord) aliveBots
+        in not $ any (\b -> _pos b == coord) aliveBots
   let forkNextLine = do
         let nd = (NearDiff 0 0 1)
         unsafePerformIO $ do
           putStrLn $ "Root bot decided to fork itself to: " ++ (show $ nearPlus (_pos myself) nd)
-          let myBid = traceShowId $ trace "My BID calculated:" $ _bid myself
+          let myBid = _bid myself
           return $ do
-            issueFission myBid (trace "Issuing root fork next" 0) nd
+            issueFission myBid 0 nd
             return ()
   unsafePerformIO $ do
     putStrLn $ "Root bot detected itself: " ++ show myself
     putStrLn $ "Preferred bot count: " ++ show preferredBotCount
     putStrLn $ "Alive bot count: " ++ show aliveBotCount
-    return $ when (preferredBotCount > aliveBotCount && isNextLineFree) forkNextLine
+    let rootBotPreferredPlace = aboveStartOfLine model layer $ head linesToFill
+    putStrLn $ "Required position for root bot: " ++ show rootBotPreferredPlace
+    if preferredBotCount > aliveBotCount && isNextLineFree
+      then return forkNextLine
+      else if _pos myself /= rootBotPreferredPlace
+        then do
+          putStrLn $ "Root bot decided to move to " ++ show rootBotPreferredPlace
+          return $ do move (_bid myself) rootBotPreferredPlace
+        else do
+          putStrLn "Root bot decided to do nothing"
+          return $ do return ()
 
 getAssignedLines :: [BotState] -> [Word8] -> [(BotState, Word8)]
 getAssignedLines bots lines =
-  let botCount = traceShowId $ trace "getAssignedLines.childCount" $ getPreferredTotalBotCount $ fromIntegral $ length lines
-      lastLines = traceShowId $ trace "getAssignedLines.lastLines" $ drop (length lines - fromIntegral botCount) lines
-      children = traceShowId $ trace "getAssignedLines.children" $ filter (not . isRootBot) bots
-      sortedChildren = traceShowId $ trace "getAssignedLines.sortedChildren" $ List.sortBy (comparing getMyLine) children
-  in zip sortedChildren lastLines
+  let rootBot = head $ filter isRootBot bots
+      botCount = getPreferredTotalBotCount $ fromIntegral $ length lines
+      lastLines = drop (length lines - fromIntegral botCount) lines
+      children = filter (not . isRootBot) bots
+      sortedChildren = List.sortBy (comparing getMyLine) children
+  in (rootBot, head lines) : zip sortedChildren lastLines
   where getMyLine bot = 
           let (_, _, z) = _pos bot
           in z
 
-startOfLine :: ModelFile -> Word8 -> Word8 -> P3
-startOfLine model layer line =
+aboveStartOfLine :: ModelFile -> Word8 -> Word8 -> P3
+aboveStartOfLine model layer line =
   let resolution = mfResolution model
       matrix = mfMatrix model
       coords = map (\x -> (x, layer, line)) [0..resolution - 1]
-  in head $ filter (\p -> getVoxel matrix p == Full) coords
+      startOfLine = head $ filter (\p -> getVoxel matrix p == Full) coords
+      (x, y, z) = startOfLine
+  in (x, y + 1, z)
 
 childBotAdjustActions :: Word8 -> [Word8] -> Generator ()
 childBotAdjustActions layer lines = do
   aliveBots <- getCurrentlyAliveBots
-  let assignedLines = traceShowId $ trace "assignedLines:" $ getAssignedLines aliveBots lines
-  let childBotAssignments = traceShowId $ trace "children calculated:" $ filter (not . isRootBot . fst) assignedLines
+  let assignedLines = getAssignedLines aliveBots lines
+  let childBotAssignments = filter (not . isRootBot . fst) assignedLines
   model <- gets gsModel
-  let moveToLine bot lineNum = do
-        move (_bid bot) $ startOfLine model layer lineNum
+  let moveToLine bot lineNum =
+        let target = aboveStartOfLine model layer lineNum
+        in unsafePerformIO $ do
+            putStrLn $ "Child bot decided to move itself to " ++ show target
+            return $ do move (_bid bot) target
   forM_ childBotAssignments $ \(bot, lineNum) -> do
-    let inPlace = getLineNum bot == lineNum
-    when (not inPlace) $ moveToLine bot lineNum
-    where getLineNum bot =
-            let (_, _, z) = _pos bot
-            in z
+    let inPlace = _pos bot == aboveStartOfLine model layer lineNum
+    unsafePerformIO $ do
+      putStrLn $ "Preparing child adjust action for " ++ show bot
+      putStrLn $ "Is bot in place? " ++ show inPlace
+      return $ do
+        when (not inPlace) $ moveToLine bot lineNum
+        where getLineNum bot =
+                let (_, _, z) = _pos bot
+                in z
 
 allInPosition :: Word8 -> [Word8] -> Generator Bool
 allInPosition layer lines = do
   aliveBots <- getCurrentlyAliveBots
   let assignedLines = getAssignedLines aliveBots lines
   model <- gets gsModel
-  let result = traceShowId $ all (\(bot, lineNum) -> _pos bot == startOfLine model layer lineNum) assignedLines
-  return result
+  let result = all (\(bot, lineNum) -> _pos bot == aboveStartOfLine model layer lineNum) assignedLines
+  unsafePerformIO $ do
+    putStrLn "Checking whether everyone is in position..."
+    putStrLn $ "Assigned positions for everyone: " ++ (show $ map (\(_, lineNum) -> aboveStartOfLine model layer lineNum) assignedLines)
+    putStrLn $ "Actual positions: " ++ show aliveBots
+    putStrLn $ "Is everyone in place? " ++ show result
+    return $ do
+      return result
 
 adjustBots :: Word8 -> [Word8] -> Generator ()
 adjustBots layer lines = do
-  rootBotAdjustAction lines
-  childBotAdjustActions layer lines
-  step
-  proceed <- allInPosition layer lines
-  if not $ traceShowId $ trace "proceed:" proceed
-    then adjustBots layer lines
-    else return ()
+  when (not $ null lines) $ do
+    currentStep <- gets gsStepNumber
+    unsafePerformIO $ do
+      putStrLn $ "Adjusting bots, step " ++ show currentStep ++ ", layer " ++ show layer
+      return $ do
+        childBotAdjustActions layer lines
+        rootBotAdjustAction layer lines
+        step
+        proceed <- allInPosition layer lines
+        if not proceed
+          then adjustBots layer lines
+          else unsafePerformIO $ do
+            putStrLn $ "Successfully adjusted for layer " ++ show layer
+            return $ do return ()
 
 parallelFillLayer :: Word8 -> Generator ()
 parallelFillLayer layerNum = do
   raiseAll $ layerNum + 1
   layerLines <- getLayerLines layerNum
-  adjustBots layerNum (trace "layerLines ok" layerLines)
+  adjustBots layerNum layerLines
   -- TODO: parallelFillLines
+  -- TODO: fuseRedundantBots
 
 parallelFill :: Generator ()
 parallelFill = do
   r <- gets (mfResolution . gsModel)
-  forM_ [0 .. (traceShowId r)-2] $ \y -> do
+  forM_ [0 .. r-2] $ \y -> do
     parallelFillLayer y
   -- TODO: mergeAll
   -- TODO: home
