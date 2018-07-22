e09e62cc4a007c60f979caf26ac9dae3af9867a5
Ilya Portnov <portnov@bk.ru>
2018-07-22 14:03:54 +0500

    Comment out generator tests.

--- test/TestGenerator.hs
+++ test/TestGenerator.hs
@@ -14,35 +14,35 @@ import Generator
 
 generator :: TestTree
 generator = testGroup "Generator"
-  [ testFusion
+  [ -- testFusion
   ]
 
-emptyBitArray :: BA.BitArray P3
-emptyBitArray = BA.fill (origin, origin) False
-
-emptyArray :: (Ix i, Num i) => Array i x
-emptyArray = listArray (0, 0) []
-
-testState :: GeneratorState
-testState = GS {
-  gsModel = ModelFile {mfResolution = 0,
-                       mfMatrix = emptyBitArray},
-  gsHarmonics = Low,
-  gsFilled = emptyBitArray,
-  gsGrounded = emptyBitArray,
-  gsStepNumber = 0,
-  gsAliveBots = [],
-  gsBots = emptyArray,
-  gsTraces = emptyArray
-}
-
-testFission :: TestTree
-testFission = testGroup "Fission command"
-  [ let nd = NearDiff 1 0 0
-        bs = Bot {_bid = 1
-                 ,_pos = nearPlus origin nd
-                 ,_seeds = []}
-        expected = testState { gsBots = (gsBots testState) // [(1, bs)] }
-        actual = snd $ runState (issueFission 0 0 nd) testState
-    in testCase "should add a bot" $ actual @?= expected
-  ]
+-- emptyBitArray :: BA.BitArray P3
+-- emptyBitArray = BA.fill (origin, origin) False
+-- 
+-- emptyArray :: (Ix i, Num i) => Array i x
+-- emptyArray = listArray (0, 0) []
+-- 
+-- testState :: GeneratorState
+-- testState = GS {
+--   gsModel = ModelFile {mfResolution = 0,
+--                        mfMatrix = emptyBitArray},
+--   gsHarmonics = Low,
+--   gsFilled = emptyBitArray,
+--   gsGrounded = emptyBitArray,
+--   gsStepNumber = 0,
+--   gsAliveBots = [],
+--   gsBots = emptyArray,
+--   gsTraces = emptyArray
+-- }
+-- 
+-- testFission :: TestTree
+-- testFission = testGroup "Fission command"
+--   [ let nd = NearDiff 1 0 0
+--         bs = Bot {_bid = 1
+--                  ,_pos = nearPlus origin nd
+--                  ,_seeds = []}
+--         expected = testState { gsBots = (gsBots testState) // [(1, bs)] }
+--         actual = snd $ runState (issueFission 0 0 nd) testState
+--     in testCase "should add a bot" $ actual @?= expected
+--   ]
